ba6acdad5e8a581e0c58bba2ba7b30cd3854fe7f
Nsj <paulodatf1212@gmail.com>
2026-02-04 14:37:57 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'pedeai-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Instala
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+// Ativa
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
