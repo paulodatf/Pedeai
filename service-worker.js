9720cd4c048db62e8d8d257eaacf236d46eb5d9f
Nsj <paulodatf1212@gmail.com>
2026-02-04 15:14:42 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,10 @@
 const CACHE_NAME = 'pedeai-v1';
 const FILES_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  'index.html',
+  'manifest.json',
+  'style.css',
+  'icons/icon-192.png',
+  'icons/icon-512.png'
 ];
 
 // Instala
